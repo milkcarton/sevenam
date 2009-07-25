a46b0139434eabe0a10645263477419a6b8ae7d5
Jelle Vandebeeck <jelle@fousa.be>
2009-07-25 19:59:28 +0000

    Remove old project files

---
+++ Classes/SevenAmViewController.h
@@ -0,0 +1,16 @@
+//
+//  SevenAmViewController.h
+//  SevenAm
+//
+//  Created by Jelle Vandebeeck on 25/07/09.
+//  Copyright milkcarton 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SevenAmViewController : UIViewController {
+
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SevenAm
+//
+//  Created by Jelle Vandebeeck on 25/07/09.
+//  Copyright milkcarton 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
